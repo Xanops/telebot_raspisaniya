93216ac5fabd413d6456390ee6d3ffd6c41a646d
Xanos <pomerantsevpeter@gmail.com>
2021-12-21 19:44:40 +0300

    Turning excel.py into function and calling it from main_messages.py def pokas_rasp. Not completed? Egor'll continue.

--- src/excel.py
+++ src/excel.py
@@ -1,26 +1,9 @@
-from openpyxl import Workbook, load_workbook
+from openpyxl import load_workbook
 
-wb = load_workbook("rasp.xlsx")
-wbb = wb.active
-week = "нечет"
-clas = "10"
-letter = "П2"
-args = {"week": week, "class": clas, "letter": letter}
-rasp = wb[f'{args["class"]}-е кл {args["week"]}']
-bigletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-              'V', 'W', 'X', 'Y', 'Z']
-column = ''
-columnn = 0
-for i in range(1000):
-    if rasp[f"{bigletters[i]}9"].value == f"{clas}{letter}":
-        column = bigletters[i]
-        columnn = i
-        break
-raspisanie = '''
-день                        |время| пара, кабинет, учитель
-----------------------
-'''
-days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота']
+firstpara = ''
+secondpara = ''
+thirdpara = ''
+fourthpara = ''
 
 
 def para(value):
@@ -35,40 +18,61 @@ def para(value):
         fourthpara = value
 
 
-count = 0
-for j in range(6):
-    firstpara = ''
-    secondpara = ''
-    thirdpara = ''
-    fourthpara = ''
-    paru = [11, 13, 16, 18, 21, 23, 26, 28, 31, 33, 36, 38, 42, 44, 47, 49, 52, 54, 57, 59, 62, 64, 67]
-    for i in range(4):
-        number = paru[count]
-        if rasp[f'{column}{number}'].value == "нет":
-            para(' ')
-        elif rasp[f'{column}{number}'].value == None:
-            if rasp[f'{bigletters[columnn - 1]}{number}'].value == None:
-                para(' ')
-            else:
-                para(f"{rasp[f'{bigletters[columnn - 1]}{number}'].value} {rasp[f'{bigletters[columnn - 1]}{number + 1}'].value}")
-        else:
-            para(f"{rasp[f'{column}{number}'].value} {rasp[f'{column}{number + 1}'].value}")
-        count += 1
-        if count > 22:
+def print_rasp():
+    wb = load_workbook("rasp.xlsx")
+    week = "нечет"
+    clas = "10"
+    letter = "П2"
+    args = {"week": week, "class": clas, "letter": letter}
+    rasp = wb[f'{args["class"]}-е кл {args["week"]}']
+    bigletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                  'U', 'V', 'W', 'X', 'Y', 'Z']
+    column = ''
+    columnn = 0
+    for i in range(1000):
+        if rasp[f"{bigletters[i]}9"].value == f"{clas}{letter}":
+            column = bigletters[i]
+            columnn = i
             break
-    if j == 5:
-        raspisanie += f'''                            |08:45| {firstpara}
-                            |10:45| {secondpara}          
-{days[j]}               
-                            |13:05| {thirdpara}
+    raspisanie = '''день                        |время| пара, кабинет, учитель
 ----------------------
 '''
-    else:
-        raspisanie += f'''                            |08:45| {firstpara}
-                            |10:45| {secondpara}          
-{days[j]}               
-                            |13:05| {thirdpara}
-                            |14:55| {fourthpara}
-----------------------
-'''
-print(raspisanie)
+    days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота']
+
+    count = 0
+    for j in range(6):
+        paru = [11, 13, 16, 18, 21, 23, 26, 28, 31, 33, 36, 38, 42, 44, 47, 49, 52, 54, 57, 59, 62, 64, 67]
+        for i in range(4):
+            number = paru[count]
+            if rasp[f'{column}{number}'].value == "нет":
+                para(' ')
+            elif rasp[f'{column}{number}'].value is None:
+                if rasp[f'{bigletters[columnn - 1]}{number}'].value is None:
+                    para(' ')
+                else:
+                    para(f"{rasp[f'{bigletters[columnn - 1]}{number}'].value} {rasp[f'{bigletters[columnn - 1]}{number + 1}'].value}")
+            else:
+                para(f"{rasp[f'{column}{number}'].value} {rasp[f'{column}{number + 1}'].value}")
+            count += 1
+            if count > 22:
+                break
+        if j == 5:
+            raspisanie += f'''                            |08:45| {firstpara}
+                           |10:45| {secondpara}          
+    {days[j]}               
+                           |13:05| {thirdpara}
+    ----------------------
+    '''
+        else:
+            raspisanie += f'''                       |08:45| {firstpara}
+                           |10:45| {secondpara}          
+    {days[j]}               
+                           |13:05| {thirdpara}
+                           |14:55| {fourthpara}
+    ----------------------
+    '''
+    print(raspisanie)
+    # return raspisanie
+
+
+print_rasp()
